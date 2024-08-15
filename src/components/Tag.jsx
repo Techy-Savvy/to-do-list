9a7fa8a18a722bf3483cfde6384893f3e77fde38
Ayaon-Norhani <norhani.ayaon@gmail.com>
2024-08-15 11:09:04 +0800

    Changed tag colors

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import "./Tag.css";
 
 const Tag = ({ tagName, selectTag, selected }) => {
+  // const tagStyle = {
+  //   Frontend: { backgroundColor: "#FFDD95" },
+  //   Backend: { backgroundColor: "#C5EBAA" },
+  //   Linux: { backgroundColor: "#80C4E9" },
+  //   Devops: { backgroundColor: "#FFAD60" },
+  //   Python: { backgroundColor: "#D1E9F6" },
+  //   default: { backgroundColor: "#f9f9f9" },
+  // };
+
   const tagStyle = {
-    Frontend: { backgroundColor: "#FFDD95" },
-    Backend: { backgroundColor: "#C5EBAA" },
-    Linux: { backgroundColor: "#80C4E9" },
-    Devops: { backgroundColor: "#EECAD5" },
+    Frontend: { backgroundColor: "#FFAD60" },
+    Backend: { backgroundColor: "#FFDD95" },
+    Linux: { backgroundColor: "#C5EBAA" },
+    Devops: { backgroundColor: "#6FEDD6" },
     Python: { backgroundColor: "#D1E9F6" },
     default: { backgroundColor: "#f9f9f9" },
   };
